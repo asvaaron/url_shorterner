0e5422cb2f38ab4666f88d8f776f5960d8ee8aae
Aaron <asvaaron@gmail.com>
2019-07-16 20:12:29 -0600

    #2:Add new table for storing shorter urls:Add methods to the url controller
    Add validation for url not null value
    Add after save method for encoding to base62 the long url. This encoding includes values from 0-9,a-z,A-Z. The max number of character is 62.
    Add function for finding url using the short url
    Add function for ordering url by times_accessed, define limit using argument

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,31 @@
+require 'base62-rb'
+
 class Url < ApplicationRecord
+  validates :url, presence: true
+
+  after_save :encode_url
+
+  def self.fetch_web_page(url_string)
+      response = HTTParty.get(url_string)
+      JSON.parse(response.body)
+  end
+  # Get top urls ordered by most times_accessed
+  def self.get_top(top_limit)
+      self.order('times_accessed DESC').limit(top_limit)
+  end
+
+  def self.get_entire_url_decoded(short_url)
+      found = self.find_by short_url: short_url
+      found.url
+  end
+
+  private
+
+  def encode_url
+    self.update_columns(
+        short_url: Base62.encode(self.id),
+        times_accessed: 0
+    )
+    true
+  end
 end
