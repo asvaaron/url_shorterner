7c6b5ddbf52833fa2153b4257ed987da714b081c
Aaron <asvaaron@gmail.com>
2019-07-17 19:59:18 -0600

    #2:Add new table for storing shorter urls:Changes in Url model
    Add comments for the validations used
    Add validation for urls, not local, define schemas (https, http)
    Add method for fetching title tag from a Website
    Add comments to internal class functions

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,14 +1,15 @@
 require 'base62-rb'
 
 class Url < ApplicationRecord
+  # Validations for url attribute not null, unique and valid url
   validates :url, presence: true
   validates_uniqueness_of :url
+  validates :url, url: { no_local: true, schemas: %w(https http)}
 
   after_save :encode_url
 
-  def self.fetch_web_page(url_string)
-      response = HTTParty.get(url_string)
-      JSON.parse(response.body)
+  def self.fetch_web_page_title(url_string)
+      return Nokogiri::HTML::Document.parse(HTTParty.get(url_string).body).title
   end
   # Get top urls ordered by most times_accessed
   def self.get_top(top_limit)
@@ -16,6 +17,7 @@ class Url < ApplicationRecord
   end
 
   def self.get_entire_url_decoded(short_url)
+      # Get entire url using the short_url stored
       url_founded = self.find_by short_url: short_url
       if !url_founded.nil?
         url_founded.increment!(:times_accessed)
@@ -26,6 +28,8 @@ class Url < ApplicationRecord
   private
 
   def encode_url
+    # Encode id using a base62 encoder valid values [0-9a-zA-Z]
+    # Initialize times accessed to 0
     self.update_columns(
         short_url: Base62.encode(self.id),
         times_accessed: 0
