ee770d2fb28b85478387b54a304db174fe5d6d64
Aaron <asvaaron@gmail.com>
2019-07-20 23:40:23 -0600

    #2:Add new table for storing shorter urls:Create new FetchUrl Worker
    Declare a new Class as a Sidequik worker class, create perform method fot fetching
    Site title using the url inserted, use function declared in the model

---
+++ app/workers/fetch_url_worker.rb
@@ -0,0 +1,8 @@
+class FetchUrlWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false, queue: 'default'
+  def perform(url_id, *args)
+    puts "Changed url id:#{url_id}"
+    Url.fetch_web_page_title(url_id)
+  end
+end
