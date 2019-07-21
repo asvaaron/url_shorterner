a9314620f6525a55c6f1f678dd8481840a6432be
Aaron <asvaaron@gmail.com>
2019-07-21 11:36:13 -0600

    #3:Fetch the title tah from the URL:Add initializer for sidekiq
    Configure sidekiq for using local redis database
    Change retry number for fetch url worker

--- app/workers/fetch_url_worker.rb
+++ app/workers/fetch_url_worker.rb
@@ -1,6 +1,6 @@
 class FetchUrlWorker
   include Sidekiq::Worker
-  sidekiq_options retry: false, queue: 'default'
+  sidekiq_options retry: 2, queue: 'default'
   def perform(url_id, *args)
     puts "Changed url id:#{url_id}"
     Url.fetch_web_page_title(url_id)
