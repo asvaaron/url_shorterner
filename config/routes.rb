e044ef99809d8e6e058d6f0ee240a826033102b3
Aaron <asvaaron@gmail.com>
2019-07-20 23:47:59 -0600

    #3:Fetch the title tag from the URL:Changes in routes field
    Add requirements for displaying a sidekiq monitor endpoint
    Define new api routes in order to prevent colisions with some shortened links

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# Load Resque Server
+require 'resque/scheduler'
+require 'resque/scheduler/server'
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Rails.application.routes.draw do
   # namespace :api do
   #   namespace :v1 do
@@ -9,8 +15,11 @@ Rails.application.routes.draw do
   #     end
   #   end
   # end
-  # Define Url Routes using api/v1 namespace
-  get '/top' , to: "api/v1/urls#top_100"
+  #Define Url Routes using api/v1 namespace
+
+  mount Sidekiq::Web => 'api/sidekiq'
+  get 'api/v1/top' , to: "api/v1/urls#top_100"
+  get 'api/v1/urls' , to: "api/v1/urls#index"
   post '/url' , to: "api/v1/urls#create"
   get '/:word', to: "api/v1/urls#decode_url"
 
