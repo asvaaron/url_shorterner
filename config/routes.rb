a998bcff53eca9da0b837219ebf06795288f4410
Aaron <asvaaron@gmail.com>
2019-07-21 11:52:29 -0600

    #3:Create api docs:Changes in routes fie
    Add swagger api and ui engine to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # namespace :api do
   #   namespace :v1 do
   #     resources :urls do
