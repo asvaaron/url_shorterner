ff2c0b67811a9f7702574299fbc18cf36bc4def9
Aaron <asvaaron@gmail.com>
2019-07-23 23:10:01 -0600

    #6:Create Base62 encoder from scratch:Changes in lib 62 encoder file
    Add additional comments to limit the total characters
    Add additional comments of how to use regrex
    Remove saving mod variable, and use one line
    Remove floor function and divide using divider shortcut
    Change number to variables for a better understanding

--- lib/base_encoders.rb
+++ lib/base_encoders.rb
@@ -1,7 +1,13 @@
 module BaseEncoders
-  BASE_62_CHARACTERS = (('0'..'9').to_a + ('a'..'z').to_a + ('A'..'Z').to_a).join()
+  # Base62 Encoder
   class Base62
+    # Define all the possible BASE62 Characters
+    # from:0-9, a-z and A-Z
+    # join hole array into unique string
+    BASE_62_CHARACTERS = (('0'..'9').to_a + ('a'..'z').to_a + ('A'..'Z').to_a).join
+
     def extract_valid_base_characters(value = "")
+      # User REGREX to remove not expected characters
       value.to_s[/([0-9a-zA-Z]+)/]
     end
 
@@ -11,24 +17,24 @@ module BaseEncoders
       # Otherwise convert database
       result = ""
       while num != 0
-        mod = num % base
-        result.prepend(BASE_62_CHARACTERS[mod], "")
-        num = (num / base).floor
+        result.prepend(BASE_62_CHARACTERS[num % base], "")
+        num /= base
       end
       result
     end
 
 
-    def decode_from_base(value = 0, base = 62)
+    def decode_from_base(value_62_base = 0, base = 62)
       base_10_number_result = 0
       # Position number for the id decoded
-      digit = 0
-      # Convert value to str and iterate using
+      iterator_digit_pos = 0
+      # Convert value to string and iterate using
       # all the characters
-      value.to_s.reverse.each_char do |character|
-        value_position = BASE_62_CHARACTERS.index(character)
-        base_10_number_result += (base ** digit) * value_position
-        digit += 1
+      value_62_base.to_s.reverse.each_char do |character|
+        # Obtain BASE62 residue
+        residue_number_value = BASE_62_CHARACTERS.index(character)
+        base_10_number_result += (base ** iterator_digit_pos) * residue_number_value
+        iterator_digit_pos += 1
       end
       base_10_number_result
     end
